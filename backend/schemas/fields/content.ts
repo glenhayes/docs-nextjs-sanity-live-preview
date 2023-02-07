53b00e0b4b71a9951cabf476966d85914e5981ca
Glenhayes <hayes@glen@gmail.com>
2023-02-07 07:40:39 -0700

    add link annotations

--- backend/schemas/fields/content.ts
+++ backend/schemas/fields/content.ts
@@ -1,5 +1,7 @@
 import {defineField} from 'sanity'
 
+import {TbFileSymlink} from 'react-icons/tb'
+
 export default defineField({
   name: 'content',
   type: 'array',
@@ -7,6 +9,36 @@ export default defineField({
   of: [
     {
       type: 'block',
+      marks: {
+        annotations: [
+          {
+            name: 'internalLink',
+            type: 'object',
+            title: 'Internal link',
+            icon: TbFileSymlink,
+            fields: [
+              {
+                name: 'reference',
+                type: 'reference',
+                title: 'Reference',
+                to: [{type: 'documentation'}],
+              },
+            ],
+          },
+          {
+            name: 'link',
+            type: 'object',
+            title: 'Link',
+            fields: [
+              {
+                name: 'link',
+                type: 'url',
+                title: 'Link',
+              },
+            ],
+          },
+        ],
+      },
     },
     {
       type: 'code',
